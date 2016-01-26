f812edd9ca01f365293853d5075b8f314eb3409e
Ranjithprabhu K <ranjithprabhu.k@gmail.com>
2016-01-26 12:28:45 +0530

    Angular E-Comm files added

---
+++ app/modules/home/homeMgmt.js
@@ -0,0 +1,29 @@
+﻿/*==========================================================
+    Author      : Ranjithprabhu K
+    Date Created: 26 Jan 2016
+    Description : Base for Home module
+    
+    Change Log
+    s.no      date    author     description     
+    
+
+ ===========================================================*/
+
+var home = angular.module('home', ['ui.router', 'ngAnimate']);
+
+
+home.config(["$stateProvider", function ($stateProvider) {
+
+    //login page state
+    $stateProvider.state('home', {
+        url: '/home',
+        templateUrl: 'app/modules/home/index.html',
+        controller: 'HomeController',
+        controllerAs: 'vm',
+        data: {
+            pageTitle: 'Home'
+        }
+    });
+
+}]);
+
